d0a12e9e9ec64952efb3aff18292098652512689
nainglinnphyo <49011578+nainglinnphyo@users.noreply.github.com>
2022-02-15 13:56:56 +0630

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 2s linear infinite',
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
